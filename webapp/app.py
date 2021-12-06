b087c81b3fd03c6036a44af2db4be2cdbfc6b4de
Jason Casareno <jcasareno@csumb.edu>
2021-12-05 20:54:37 -0800

    Updated app.py and index template
    Note: Temporarily commented out certain imports that didn't allow the webpage to run
    
    app.py
    - Added Bootstrap import
    - Added initialization of Flask and Bootstrap
    - Define a route and function to render the index template
    
    index.html
    - Added "{{ bootstrap.load_css() }}"
    - Added style (center text alignment)
    - Added title name and header

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request
-from gpiozero import MotionSensor
-from picamera import PiCamera
-import datetime
-from time import sleep
+from flask_bootstrap import Bootstrap
+# from gpiozero import MotionSensor
+# from picamera import PiCamera
+# import datetime
+# from time import sleep
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
