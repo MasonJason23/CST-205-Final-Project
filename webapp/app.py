9e712c8ee1b43a1284e2e8d0d8ed04405b7d97b5
Jason Casareno <jcasareno@csumb.edu>
2021-12-08 19:59:19 -0800

    Dispay images via from PixaBay
    Using the API 'PixaBay', 3 random images + user credit is now displayed on the main page (index.html).

--- webapp/app.py
+++ webapp/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 import requests, json
 from pprint import pprint
+import random
 # from gpiozero import MotionSensor
 # from picamera import PiCamera
 # import datetime
@@ -10,20 +11,26 @@ from pprint import pprint
 my_key = '24730070-074e9d5f350c21a172695576f'
 
 payload = {
-  'key': my_key,
-  'category': 'food'
+    'key': my_key,
+    'category': 'animals'
 }
 endpoint = 'https://pixabay.com/api/'
 try:
-  r = requests.get(endpoint, params=payload)
-  data = r.json()
-  pprint(data)
+    r = requests.get(endpoint, params=payload)
+    data = r.json()
+    # pprint(data['hits'])
 except:
-  print('please try again')
+    print('please try again')
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    images = data['hits']
+    random.shuffle(images)
+    im_info1 = images[0]
+    im_info2 = images[1]
+    im_info3 = images[2]
+    return render_template('index.html', imInfo1 = im_info1, imInfo2 = im_info2, imInfo3 = im_info3)
+
