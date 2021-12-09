918bc35650f34a21a0d99c831775d369257b65d0
Jason Casareno <jcasareno@csumb.edu>
2021-12-08 17:46:04 -0800

    Implemented PixaBay API
    Implementation uses my key and prints out the image information in the terminal.

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+import requests, json
+from pprint import pprint
 # from gpiozero import MotionSensor
 # from picamera import PiCamera
 # import datetime
 # from time import sleep
+
+my_key = '24730070-074e9d5f350c21a172695576f'
+
+payload = {
+  'key': my_key,
+  'category': 'food'
+}
+endpoint = 'https://pixabay.com/api/'
+try:
+  r = requests.get(endpoint, params=payload)
+  data = r.json()
+  pprint(data)
+except:
+  print('please try again')
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
