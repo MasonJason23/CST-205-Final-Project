a9115ec2672f57559d17749ac3ef5b6011746e03
Jason Casareno <jcasareno@csumb.edu>
2021-12-10 00:45:17 -0800

    Results Page Update
    Summary: The main page now has a working search bar which can be used to search images (using PixaBay API). Results page is now displays user's searches.
    
    Updated app.py
    - created a form that displays a working search bar in index.html
    - reworked the 'results' function
    -- obtains user 'search' data
    -- based on 'search' data, sends image data when going to 'results' page
    
    Updated index.html
    - added code that inplements a form ('search' bar)
    
    Updated results.html
    - similar format as index.html
    - uses information from app.py to display (if applicable) images that the user desires

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from datetime import datetime
 import requests, json
 from pprint import pprint
 import random
@@ -7,6 +11,10 @@ import random
 # from picamera import PiCamera
 # import datetime
 # from time import sleep
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'csumb-otter'
+bootstrap = Bootstrap(app)
 
 my_key = '24730070-074e9d5f350c21a172695576f'
 
@@ -22,23 +30,42 @@ try:
 except:
     print('please try again')
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+class List(FlaskForm):
+    search = StringField(
+        'Search an animal', 
+        validators=[DataRequired()]
+    )
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    form = List()
+    if form.validate_on_submit():
+        return results(form.search.data)
+    
     images = data['hits']
     random.shuffle(images)
     im_info1 = images[0]
     im_info2 = images[1]
     im_info3 = images[2]
-    return render_template('index.html', imInfo1 = im_info1, imInfo2 = im_info2, imInfo3 = im_info3)
+    return render_template('index.html', form=form, imInfo1 = im_info1, imInfo2 = im_info2, imInfo3 = im_info3)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/results/<tag>')
-def results(tag):
+@app.route('/results')
+def results(search):
+    img_list = []
     images = data['hits']
-    return render_template('results.html')
+    image_limit = 10
+
+    for image in images:
+        if (image_limit == 0):
+            break
+        tags = image['tags'].split(",")
+        for tag in tags:
+            if (tag == search):
+                img_list.append(image)
+                image_limit -= 1
+
+    return render_template('results.html', animal=search, list=img_list)
