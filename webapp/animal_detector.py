aff7654be95de3bf00a124131a437f33acdd13ca
Chris Johnson <im_were@hotmail.com>
2021-12-11 01:38:15 -0800

    Update animal_detector.py
    Made changes to structure of code

--- webapp/animal_detector.py
+++ webapp/animal_detector.py
@@ -1,11 +1,22 @@
 import cv2
 import numpy as np
+from pprint import pprint
 
 casc_class = 'haarcascade_frontalcatface.xml'
+
 face_cascade = cv2.CascadeClassifier(casc_class)
-img = cv2.imread('group.jpg')
+
+if face_cascade.empty():
+    print('WARNING: Cascade did not load')
+
+img = cv2.imread('fillon.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 faces = face_cascade.detectMultiScale(gray, 1.1, 9)
-for (x,y,w,h) in faces:
-cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-cv2.imwrite('newgroup.png', img)
+
+pprint(faces)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imwrite('newface2.png', img)
