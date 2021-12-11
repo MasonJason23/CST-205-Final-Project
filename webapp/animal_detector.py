f670b8fd0ebe329971ae45a730e0933080c69eeb
Chris Johnson <im_were@hotmail.com>
2021-12-11 00:17:51 -0800

    added basic structure for animal detection

--- webapp/animal_detector.py
+++ webapp/animal_detector.py
@@ -1,3 +1,11 @@
 import cv2
 import numpy as np
 
+casc_class = 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(casc_class)
+img = cv2.imread('group.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.1, 9)
+for (x,y,w,h) in faces:
+cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+cv2.imwrite('newgroup.png', img)
