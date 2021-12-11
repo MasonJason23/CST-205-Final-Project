0728e28a0ef80796bd2957b1f9d555db6c29275d
Chris Johnson <im_were@hotmail.com>
2021-12-11 01:17:30 -0800

    Update animal_detector.py
    Fixed line that had wrong filename.

--- webapp/animal_detector.py
+++ webapp/animal_detector.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-casc_class = 'haarcascade_frontalface_default.xml'
+casc_class = 'haarcascade_frontalcatface.xml'
 face_cascade = cv2.CascadeClassifier(casc_class)
 img = cv2.imread('group.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
