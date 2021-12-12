097a8998cacba37c702cfc42c7910d29024e55a7
Chris Johnson <im_were@hotmail.com>
2021-12-12 14:11:18 -0800

    Update animal_detector.py
    Added glob

--- webapp/animal_detector.py
+++ webapp/animal_detector.py
@@ -1,6 +1,10 @@
+from typing import Any
+
 import cv2
 import numpy as np
 from pprint import pprint
+from PIL import Image
+import glob
 
 casc_class = 'haarcascade_frontalcatface.xml'
 
@@ -9,12 +13,12 @@ face_cascade = cv2.CascadeClassifier(casc_class)
 if face_cascade.empty():
     print('WARNING: Cascade did not load')
 
-img = cv2.imread('fillon.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# images = np.array([cv2.imread(i) for i in glob.glob('static/*.jpeg')])
+# img = cv2.imread('fillon.jpg')
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+images = np.array([cv2.cvtColor(cv2.imread(i), cv2.COLOR_BGR2GRAY) for i in glob.glob('static/*.jpeg')])
 
-faces = face_cascade.detectMultiScale(gray, 1.1, 9)
-
-pprint(faces)
+faces = np.array([face_cascade.detectMultiScale(i, 1.1, 9) for i in images])
 
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
