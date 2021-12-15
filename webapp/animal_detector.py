0d1d15315a2d86a2bfca51ebd9ee780289841cae
Chris Johnson <im_were@hotmail.com>
2021-12-15 01:12:58 -0800

    Prints list of images with cats

--- webapp/animal_detector.py
+++ webapp/animal_detector.py
@@ -1,7 +1,5 @@
 import cv2
 import numpy as np
-# from pprint import pprint
-# from PIL import Image
 import glob
 
 casc_class = 'haarcascade_frontalcatface.xml'
@@ -13,15 +11,11 @@ if face_cascade.empty():
 
 images = np.array(glob.glob('static/img/*.jpg'))
 
-print(images)
-
 grays = np.array([cv2.cvtColor(cv2.imread(i), cv2.COLOR_BGR2GRAY) for i in images])
 faces = np.array([face_cascade.detectMultiScale(i, 1.1, 9) for i in grays])
 
-print(len(faces))
-
-count = 0
-for image in faces:
-    if len(image) != 0:
-        count += 1
-print(f'Found {count} cats')
+results = []
+for i in range(len(faces)):
+    if len(faces[i]) != 0:
+        results.append(images[i])
+print(results)
